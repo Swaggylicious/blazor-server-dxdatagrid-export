49a85870fa2c9938482875538c55fa47d0a0f176
wei2.yeo@gmail.com <wei2.yeo@gmail.com>
2021-02-10 21:55:17 +0800

    change to dotnet core from 5->3.1

--- CS/DxDataGridExportingWithReports/Startup.DevExpress.cs
+++ CS/DxDataGridExportingWithReports/Startup.DevExpress.cs
@@ -7,7 +7,7 @@
 //
 // <Project Sdk="Microsoft.NET.Sdk.Web">
 //  <PropertyGroup>
-//    <TargetFramework>net5.0</TargetFramework>
+//    <TargetFramework>netcoreapp3.1</TargetFramework>
 //    <DxExtendStartupHost>False</DxExtendStartupHost>
 //  </PropertyGroup>
 //</auto-generated>
